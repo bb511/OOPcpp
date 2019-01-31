0feefb116f5c6c91297bdd2815dc515463001633
bb511 <patrickodagiu@gmail.com>
2019-01-31 11:24:04 +0000

    Added gitignore to ignore the output file and completed Assignment 1.

--- Assignment1.cpp
+++ Assignment1.cpp
@@ -6,6 +6,8 @@ Date: 30/01/2019
 
 #include <iostream>
 #include <iomanip>
+#include <math.h>
+#include <limits>
 using namespace std;
 
 struct Transition{
@@ -20,70 +22,101 @@ struct Transition{
 };
 
 int readAtomicNumber(){
-	int Z;
-	cout<<"Please input the atomic number (Z) of the atom:\n"
+	// Reads the atomic number and tests for valid input.
+	unsigned int Z;
+	cout<<"Please input the atomic number (Z) of the atom:\n";
 	cin>>Z;
 	while(cin.fail() || Z == 0){
 		cout<<"Sorry, your input was not valid, please try again:\n";
-		cin.clear(); cin.ignore(10000, '\n'); cin>>Z;
+		cin.clear(); cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		cin>>Z;
 	}
 	return Z;
 }
 
 int readInitialQuantumNumber(){
-	int nInitial;
-	cout<<"Please input initial quantum number (nInitial) of the transition:\n"
+	// Reads the initial quantum number and tests for valid input.
+	unsigned int nInitial;
+	cout<<"Please input initial quantum number (nInitial) of the transition:\n";
 	cin>>nInitial;
 	while(cin.fail() || nInitial == 0){
 		cout<<"Sorry, your input was not valid, please try again:\n";
-		cin.clear(); cin.ignore(10000, '\n'); cin>>nInitial;
+		cin.clear(); cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		cin>>nInitial;
 	};
 	return nInitial;
 }
 
-int readFinalQuantumNumber(){
-	int nFinal;
-	cout<<"Please input the final quantum number (nFinal) of the transition:\n"
+int readFinalQuantumNumber(unsigned int nInitial){
+	// Reads the final quantum number and tests for valid input.
+	unsigned int nFinal;
+	cout<<"Please input the final quantum number (nFinal) of the transition:\n";
 	cin>>nFinal;
-	while(cin.fail() || nFinal == 0){
+	while(cin.fail() || nFinal == 0 || (nInitial < nFinal)){
 		cout<<"Sorry, your input was not valid, please try again:\n";
-		cin.clear(); cin.ignore(10000, '\n'); cin>>nFinal;
+		cin.clear(); cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		cin>>nFinal;
 	}
 	return nFinal;
 }
 
 int readDesiredFormat(){
+	// Reads the desired energy units and tests for valid input.
 	char energyUnits;
-	cout<<"What units should the energy be in?:\n"
+	cout<<"What units should the energy be in, joules or eV? (J/e)\n";
 	cin>>energyUnits;
-	while(cin.fail() || (energyUnits != 'J' || energyUnits != 'e')){
+	while(cin.fail() || (energyUnits != 'J' && energyUnits != 'e')){
 		cout<<"Sorry, your input was not valid, please try again:\n";
-		cin.clear(); cin.ignore(10000, '\n'); cin>>energyUnits;
+		cin.clear(); cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		cin>>energyUnits;
 	}
 	return energyUnits;
 }
 
 Transition readUserData(){
-	// Function that reads the user input (i.e. fills the struct Transition).
+	// Reads the input that the user needs to provide about the
+	// transition he wants the energy calculated for.
 
 	Transition transitionData;
-	cout<<"These are instructions";
+	cout<<"Welcome to this simple single electron atom transition calculator. "
+		  "Valid inputs for atomic number, initial quantum number and final "
+		  "quantum number are non-zero positive integers. For the energy units, "
+		  "this tool offers either Joules or electron volts.\n";
+	transitionData.Z = readAtomicNumber();
+	transitionData.nI = readInitialQuantumNumber();
+	transitionData.nF = readFinalQuantumNumber(transitionData.nI);
+	transitionData.energyUnits = readDesiredFormat();
 
 	return transitionData;
 }
 
 float calculateEnergy(Transition transitionData){
-	// Function that calculates the energy of a transition
+	// Calculates the energy of the transition described by transitionData.
+	const float rydbergEnergy{13.6};
+	return rydbergEnergy*pow(transitionData.Z,2)*(1/pow(transitionData.nF,2) -
+		   1/pow(transitionData.nI, 2));
+}
 
+void outputFormattedEnergy(float energy, char energyUnits){
+	// Formats and outputs the energy fo the transition.
+	const float elementaryCharge{1.602*pow(10,-19)};
+	cout<<"The energy of the transition is: "<<setprecision(3);
+	if(energyUnits == 'J')
+		cout<<energy*elementaryCharge<<" J\n\n";
+	else
+		cout<<energy<<" eV\n\n";
 }
 
 char askForRepeat(char repeat){
-	cout<<"Would you like to repeat the calculation
-	       with a different input? (y/n)";
+	// Asks if the user wants to repeat the calculation with a different input
+	// and checks if the input is valid.
+	cout<<"Would you like to repeat the calculation "
+	      "with a different input? (y/n)\n";
 	cin>>repeat;
-	while(repeat != 'y' || repeat != 'n'){
-		cout<<"Please enter either 'y' or 'n' (without the apostrophes)";
-		cin<<repeat;
+	while(repeat != 'y' && repeat != 'n'){
+		cout<<"Please enter either 'y' or 'n' (without the apostrophes) ";
+		cin.clear(); cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		cin>>repeat;
 	};
 
 	return repeat;
@@ -93,11 +126,14 @@ int main(){
 
 	Transition transitionData;
 	char repeat{'y'};
+	float energy;
 
 	while(repeat != 'n'){
-
 		transitionData = readUserData();
-		calculateEnergy(transitionData)
-		askForRepeat(repeat);
+		energy = calculateEnergy(transitionData);
+		outputFormattedEnergy(energy, transitionData.energyUnits);
+		repeat = askForRepeat(repeat);
 	};
+
+	return 0;
 }
